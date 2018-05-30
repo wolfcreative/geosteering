71bc7aa2344cfd1b159185764380157d0910dca9
Hiddenski <loonysnow@gmail.com>
2018-05-30 20:59:52 +0300

    update

--- js/all.js
+++ js/all.js
@@ -1,13 +1,3 @@
 $(function() {
-  var form_title;
-
-  $( ".premises .uk-button" ).click(function() {
-    form_title = $(this).closest('.wrapper').children('h3').text()
-    $('#modal-call input[name=modalinfo]').val('Помещение:' + form_title);
-  });
-
-  $( ".results .uk-button" ).click(function() {
-    form_title = $(this).closest('.info').find('h3').text()
-    $('#modal-call input[name=modalinfo]').val(form_title);
-  }); 
+  
 });
